f4018025680578c4ee5d4201034ffb3aa291ac66
ChristineAkinyi <christinneakinyi@gmail.com>
2024-05-09 19:41:36 +0300

    Add changes

--- asynchronous.js
+++ asynchronous.js
@@ -1,4 +1,4 @@
-// // Write an asynchronous function that accepts a message string and a delay time in milliseconds. The function should log the message to the console after the specified delay time.
+// Question 1
 
 let promise = new Promise(function (resolve){
      setTimeout(function(){
@@ -15,7 +15,7 @@ let promise = new Promise(function (resolve){
  logMessageAfterDelay();
 
 
-// You have an array of user IDs and a function getUserData(id) that returns a Promise with user data when given a user ID. Write an asynchronous function that fetches and logs the data for each user ID one by one, in sequence.
+//Question 2
 async function getUserData(id) {
     return new Promise((resolve) => {
       setTimeout(() => {
@@ -45,7 +45,7 @@ async function getUserData(id) {
 fetchAndLogUserData(userIds);
 
 
-// You have an asynchronous function performTask() that returns a Promise. The Promise resolves if the task is successful and rejects if there's an error. Write a function that calls performTask() and logs a custom success message if the task is successful, and a custom error message if there's an error.
+// Question 3
 const result = true;
 const ourPromise = new Promise((resolve, reject)=>{
     if(result){
@@ -99,11 +99,11 @@ async function executeWithRetry(taskName, retries, failureProbability) {
     try {
     const result = await unstableTask(taskName, failureProbability);
     console.log(result);
-    return; // exit function if task succeeds
+    return;
     } catch (error) {
     console.log(error);
     if (i === retries) {
-    throw new Error(`Failed after ${retries} retries`);
+    throw new Error(`Task has failed after ${retries} retries`);
     }
     }
     }
